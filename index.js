3067dc9b454a6d073fa5ddf6a7aa35614c8f86f3
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-08 11:15:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const findMatching = (arr, string) => {
   return arr.filter(x => {
-    return x.toLowerCase() === string.toLowerCase()
-  })
+    return x.toLowerCase() === string.toLowerCase();
+  });
 };
 
 const fuzzyMatch = (arr, string) => {
   return arr.filter(x => {
-    return x.startsWith(string)
-  })
+    return x.startsWith(string);
+  });
 };
