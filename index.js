f92d769ea7970680172882c4fd56e4d226826e56
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-08 11:18:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const findMatching = (arr, string) => {
+  return arr.filter(x => {
+    return x.toLowerCase() === string.toLowerCase();
+  });
+};
+
+const fuzzyMatch = (arr, string) => {
+  return arr.filter(x => {
+    return x.startsWith(string);
+  });
+};
+
+const matchName = (arr, string) => {
+  return arr.filter(x => {
+    return x.name === string
+  })
+};
